980c6a0a1cc8af6272ce3239f13a7ffff92e882a
abhikapoor <abhi.kpr1@gmail.com>
2025-11-05 12:50:19 +0100

    increased limit

---
+++ dist/libs/shared/prisma/src/client.d.ts
@@ -0,0 +1,2 @@
+import { PrismaClient } from './generated/client';
+export declare const prisma: PrismaClient<import("libs/shared/prisma/src/generated").Prisma.PrismaClientOptions, never, import("libs/shared/prisma/src/generated/runtime/library").DefaultArgs>;
